a2abad117ac483f5d87b4e19ca3080e7b6dcc544
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2022-02-16 13:01:42 +0100

    opdr 1.2

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,8 @@
+<?php
+$persoon["voornaam"] = "Willem Alexander";
+$persoon["achternaam"] = "van Oranje";
+$persoon ["leeftijd"] = "53 jaar";
+$persoon ["opleiding"] = "Software Development";
+
+echo  $persoon["voornaam"]. " " .  $persoon["achternaam"] . " " . " " . $persoon["leeftijd"] . " " . "oud en is student aan het ROC van Amsterdam aan de opleiding" . " " . $persoon["opleiding"] . ".";
+?>
