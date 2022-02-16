775948b590f25e78996f72b0671c5972d9307aaf
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2022-02-16 13:58:39 +0100

    opdr 2.1

---
+++ 2-Loops/Opdracht-2.1/loops-while.php
@@ -0,0 +1,12 @@
+<?php
+$leeftijd = 0;
+
+echo "<ul>";
+while($leeftijd<18){
+
+    echo "<li>Ik ben $leeftijd jaar, dus ik mag nog niet stemmen </li>";
+
+    $leeftijd ++;
+}
+
+echo "<li>Ik ben $leeftijd jaar, dus ik heb stemrecht! </li></ul>"; 
