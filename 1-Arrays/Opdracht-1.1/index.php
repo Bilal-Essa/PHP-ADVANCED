c8d708e34f5eaec766b09ee9b3320bda44e0832b
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2022-02-16 12:43:15 +0100

    opdr 1.1

---
+++ 1-Arrays/Opdracht-1.1/index.php
@@ -0,0 +1,9 @@
+?><?php
+$student["voornaam"] = "Bilal";
+$student ["achternaam"] = "Essalhi";
+$student ["klas"] = "1B";
+$student ["leeftijd"] = "17 jaar" ;
+
+
+echo "Mijn gegevens=" . "Voornaam=" . $student['voornaam'] . " " . "Achternaam=" . $student['achternaam'] . " " . "Klas=" . $student['klas'] . " ". "Leeftijd=" . $student['leeftijd'];
+
