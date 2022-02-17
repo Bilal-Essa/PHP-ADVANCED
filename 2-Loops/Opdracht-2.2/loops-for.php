80cfb1224ded40cff8e55b74d618aec3535c1e35
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2022-02-17 09:58:08 +0100

    opdr 2.2

---
+++ 2-Loops/Opdracht-2.2/loops-for.php
@@ -0,0 +1,32 @@
+<?php
+$x = 0;
+echo "<ul>";
+for ($x = 0; $x <18; $x++) {
+
+  echo "<li>Ik ben $x jaar, dus ik mag helaas nog niet stemmen</li>";
+
+}
+
+echo "<li>Ik ben $x jaar, dus ik heb stemrecht! </li></ul>"; 
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
