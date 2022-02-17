af7d1d7d2eb0e8d79d3283cbf5b4616ab2cefa77
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2022-02-17 13:09:49 +0100

    opdr 2.3

---
+++ 2-Loops/Opdracht-2.3/loops-foreach-deel1.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<ul>";
+$dagen = array("maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag");
+
+foreach ($dagen as $value){
+echo "<li> $value </li>";
+}                        
+
+echo "</ul>";
+
+?>
+
