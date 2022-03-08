2c6f17a960e15ac74b7084f887a4876e7409a6f5
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2022-03-08 15:10:44 +0100

    klaar 3.3

---
+++ 3-Forms/Opdracht-3.3/mijnmuziek.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form method="GET" action="mijnmuziek2.0.php">
+
+Artiest <input name="Artiest"> <br>
+
+Titel <input name="Titel"> <br>
+
+Jaar van uitkomst <input name="JAAR"> <br>
+
+Genre <input name="Genre"> <br>
+
+<button>Verstuur</button><br>
+
+
+
+
+
+</body>
+</html>
---
+++ 3-Forms/Opdracht-3.3/mijnmuziek2.0.php
@@ -0,0 +1,11 @@
+<?php
+
+echo $_GET['Artiest'] . "<br>";
+
+echo $_GET['Titel'] . "<br>";
+
+echo $_GET['JAAR'] . "<br>";
+
+echo $_GET['Genre'] . "<br>";
+
+?>
